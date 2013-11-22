8ed026ad56a09ef1608a2cf16422a2386a3c8356
Robin Verlangen <robin@us2.nl>
2013-11-22 20:35:34 +0100

    Basic test

--- src/cloudpelican/cloudpelican.go
+++ src/cloudpelican/cloudpelican.go
@@ -3,8 +3,25 @@ package cloudpelican
 // @author Robin Verlangen
 // Tool for logging data to CloudPelican directly from Go
 
+// Imports
+import (
+    "net/http"
+    "log"
+)
+
 // Write a message
-func LogMessage(x string) bool {
+func LogMessage(token string, msg string) bool {
     // @todo
     return false
+}
+
+// Request a sync
+func requestAsync(url string) bool {
+    go func() {
+        _, err := http.Get(url)
+	if err != nil {
+            log.Printf("Error while forwarding data: %s\n", err)
+        }
+    }()
+    return true
 }
--- src/cloudpelican/cloudpelican_test.go
+++ src/cloudpelican/cloudpelican_test.go
@@ -7,9 +7,10 @@ import "testing"
 
 // Test log message
 func TestLogMessage(t *testing.T) {
+    const token string = "12345"
     const in string = "Hello World"
     const out bool = true
-    if x := LogMessage(in); x != out {
+    if x := LogMessage(token, in); x != out {
         t.Errorf("LogMessage(%v) = %v, want %v", in, x, out)
     }
 }
