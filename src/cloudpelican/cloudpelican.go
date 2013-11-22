533ca6291779994166876120c8d123ace0f473fa
Robin Verlangen <robin@us2.nl>
2013-11-22 21:05:28 +0100

    Working on drain function

--- src/cloudpelican/cloudpelican.go
+++ src/cloudpelican/cloudpelican.go
@@ -12,6 +12,13 @@ import (
 // Settings
 const ENDPOINT string = "https://app.cloudpelican.com/api/push/pixel"
 
+// Monitor drain status
+routineQuit chan int = make(chan int)
+startCounter uint64 = uint64(0)
+startCounterMux sync.Mutex
+doneCounter uint64 = uint64(0)
+doneCounterMux sync.Mutex
+
 // Write a message
 func LogMessage(token string, msg string) bool {
     // @todo Write seperate func for url assembly, encoding, etc
@@ -20,15 +27,35 @@ func LogMessage(token string, msg string) bool {
     return res
 }
 
+// Drain: wait for all data pushes to finish
+func Drain() bool {
+    if startCounter > doneCounter {
+        <-routineQuit
+    }
+}
+
 // Request a sync
 // @todo Make sure all are pushed to backend before application shuts down
 func requestAsync(url string) bool {
+    // Add counter
+    startCounterMux.Lock()
+    startcounter++
+    startCounterMux.Unlock()
+
+    // Launch
     go func() {
+        // Request url
         _, err := http.Get(url)
         log.Println(url)
 	if err != nil {
             log.Printf("Error while forwarding data: %s\n", err)
         }
+
+        // Done counter
+        doneCounterMux.Lock()
+        doneCounter++
+        doneCounter.Unlock()
+        // @todo Check whether dif between started and done is = 0, if so, drop a message in the routineQuit
     }()
     return true
 }
