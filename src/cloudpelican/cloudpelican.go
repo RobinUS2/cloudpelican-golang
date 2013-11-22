8c1ab3eb614eef1114571b5591dc8cde41a6b617
Robin Verlangen <robin@us2.nl>
2013-11-22 20:11:25 +0100

    First stub

---
+++ src/cloudpelican/cloudpelican.go
@@ -0,0 +1,10 @@
+package cloudpelican
+
+// @author Robin Verlangen
+// Tool for logging data to CloudPelican directly from Go
+
+// Write a message
+func LogMessage(x string) bool {
+    // @todo
+    return false
+}
