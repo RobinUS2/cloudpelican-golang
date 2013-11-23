5d0af985644b5865e3a5ed719b810d0025f37d17
Robin Verlangen <robin@us2.nl>
2013-11-23 15:47:54 +0100

    Updated test

--- src/cloudpelican/cloudpelican_test.go
+++ src/cloudpelican/cloudpelican_test.go
@@ -4,13 +4,23 @@ package cloudpelican
 // Test the CloudPelican for Go libraby
 
 import "testing"
+import "cloudpelican"
 
 // Test log message
 func TestLogMessage(t *testing.T) {
-    const token string = "12345"
     const in string = "Hello World"
     const out bool = true
-    if x := LogMessage(token, in); x != out {
-        t.Errorf("LogMessage(%v) = %v, want %v", in, x, out)
+    if x := cloudpelican.LogMessage(in); x != out {
+        t.Errorf("cloudpelican.LogMessage(%v) = %v, want %v", in, x, out)
+    }
+}
+
+// Test log message with token
+func TestLogMessageWithToken(t *testing.T) {
+    const in string = "Hello World"
+    const token string = "54321"
+    const out bool = true
+    if x := cloudpelican.LogMessageWithToken(token, in); x != out {
+        t.Errorf("cloudpelican.LogMessageWithToken(%v) = %v, want %v", in, x, out)
     }
 }
